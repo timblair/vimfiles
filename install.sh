4772c0c9b1404115f3d62f86e4f21805a8cf363f
Tim Blair <tim@bla.ir>
2013-03-11 11:53:01 +0000

    Move transient files to new location
    The recent update to vim-sensible has moved the location of
    where transient files are stored: tpope/vim-sensible@6c1ed9b0

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@
 
 git clone git://github.com/timblair/vimfiles.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc && cd ~/.vim
-mkdir -p ~/.cache/vim/{swap,backup,undo}
+
+if [[ $(uname) == 'Darwin' ]]; then
+  mkdir -p ~/Library/Vim/{swap,backup,undo}
+else
+  mkdir -p ~/.local/share/vim/{swap,backup,undo}
+fi
+
 git submodule init && git submodule update
