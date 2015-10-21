0222e63a80f0f82e6da9ee15ff941362b4eaed7a
Tim Blair <tim@bla.ir>
2015-10-21 18:12:16 +0100

    Install CMake (for YCM) in install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Install MacVim as follows:
-# $ brew install macvim --env-std --override-system-vim
-#
-# Install font with Powerline patches:
-# https://github.com/Lokaltog/powerline-fonts/blob/master/InconsolataDz/Inconsolata-dz%20for%20Powerline.otf?raw=true
-
 git clone git://github.com/timblair/vimfiles.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc && cd ~/.vim
 
@@ -13,6 +7,11 @@ if [[ $(uname) == 'Darwin' ]]; then
   mkdir -p ~/Library/Vim/{swap,backup,undo}
 else
   mkdir -p ~/.local/share/vim/{swap,backup,undo}
+fi
+
+# Install CMake for YCMm (I'm on OS X, so we'll use Homebrew)
+if hash brew 2>/dev/null; then
+  $( hash cmake 2>/dev/null ) || brew install cmake
 fi
 
 vim +PlugInstall +qall
